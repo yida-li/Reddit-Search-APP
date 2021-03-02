4e36f839b9f1da31d7c7b6f941aa88288e891254
yida-li <gabrielyidali@gmail.com>
2021-03-01 21:40:43 -0500

    deployed prototype

--- redditsearchapp/src/App.js
+++ redditsearchapp/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Article from './components/Article';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('SemenRetention');
+
+  useEffect(() => {
+    fetch('https://www.reddit.com/r/' + subreddit + '.json?limit=100').then(
+      (res) => {
+        if (res.status !== 200) {
+          console.log('ERROR');
+          return;
+        }
+
+        res.json().then((data) => {
+          if (data !== null) {
+            setArticles(data.data.children);
+          }
+        });
+      }
+    );
+  }, [subreddit]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>
+          <i class='fas fa-globe-americas'></i>Enter the subreddit you would
+          like to visit<i class='fas fa-globe-americas'></i>
+        </h1>
+      </div>
+
+      <div className='App'>
+        <header className='App-header'>
+          <input
+            type='text'
+            className='input'
+            value={subreddit}
+            onChange={(e) => setSubreddit(e.target.value)}
+          />
+        </header>
+        <div className='articles'>
+          {articles != null
+            ? articles.map((article, index) => (
+                <Article key={index} article={article.data} />
+              ))
+            : ''}
+        </div>
+      </div>
     </div>
   );
 }
